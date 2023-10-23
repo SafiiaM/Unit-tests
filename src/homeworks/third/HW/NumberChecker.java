e9719a049f8649c9617ada65a005624359f1b21e
SafiiaM <107404606+SafiiaM@users.noreply.github.com>
2023-10-23 05:44:21 +0300

    HW_3

---
+++ src/homeworks/third/HW/NumberChecker.java
@@ -0,0 +1,11 @@
+package homeworks.third.HW;
+
+public class NumberChecker {
+    public boolean isOdd(int num) {
+        return num%2 == 0;
+    }
+
+    public boolean inInterval(int num) {
+        return num <= 100 && num >= 25;
+    }
+}
